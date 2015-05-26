bc29b976870b71634fe22c077ef737617770b78e
Jack Dunn <jack.dunn.nz@gmail.com>
2015-05-26 12:03:16 -0400

    [osx] Fix comment header

--- src/Main.osx.mm
+++ src/Main.osx.mm
@@ -1,4 +1,5 @@
-//  main.mm
+// Main.osx.mm
+// Functions for the final executable on OS X
 
 #import <Foundation/Foundation.h>
 
@@ -11,11 +12,11 @@ int main(int argc, const char * argv[]) {
     std::string logFilePath;
     OPENSOLVER::GetLogFilePath(&logFilePath);
     NSLog(@"%@", [NSString stringWithUTF8String:logFilePath.c_str()]);
-    
+
     double newVars[4] = { 2.5, 3.2, 4.0, 5.999 };
     double bestSolution = 4.0;
     OPENSOLVER::UpdateVars(newVars, 4, &bestSolution, true);
-    
+
     NSLog(@"Hello, World!");
   }
   return 0;
