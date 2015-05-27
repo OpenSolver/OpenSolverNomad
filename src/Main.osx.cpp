94f4b0c2616a3f90c0c69b6600ad60aad37d8c4f
Jack Dunn <jack.dunn.nz@gmail.com>
2015-05-26 23:39:26 -0400

    [osx] Replace .mm main with .cpp to ease linking

---
+++ src/Main.osx.cpp
@@ -0,0 +1,23 @@
+// Main.osx.cpp
+// Main function for the final executable on OS X
+
+#include "NomadInterface.h"
+
+#include <string>
+
+int main(int argc, const char * argv[]) {
+  if (argc == 2) {
+    
+    const char* arg = argv[argc - 1];
+    if (strcmp(arg, "-v") == 0) {
+      printf(OPENSOLVER::VERSION);
+      return EXIT_SUCCESS;
+    } else if (strcmp(arg, "-nv") == 0) {
+      printf("%s", NOMAD::VERSION.c_str());
+      return EXIT_SUCCESS;
+    }
+  } else if (argc > 2) {
+    //error
+  }
+  return OPENSOLVER::RunNomad();
+}
